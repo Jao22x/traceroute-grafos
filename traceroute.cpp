c0ab70d3ef67dccc5800da071120bbc27c5707d9
João Dalla Lana <182407@upf.br>
2026-06-13 18:13:03 -0300

    Exibir grafo

--- traceroute.cpp
+++ traceroute.cpp
@@ -23,7 +23,10 @@ int main(int argc, char*argv[]){
         cerr << "Não foi possível abrir o arquivo para leitura." << endl;
         return 1;
     } 
-   
+    
+
+//ifstream arq("input_1.log");
+
 //LER O ARQUIVO
     graph::digraph g;
     unordered_set<string> seen_arestas; //arestas existentes, nao aceita duplicadas
@@ -73,13 +76,72 @@ int main(int argc, char*argv[]){
         }
     }
 
-    cout << "--------------"
-            <<"\nCarga de Dados inserida com sucesso!" 
-                << "\nNumero de Vertices Unicos = " << vert_unicos << " | Grafo: " << g.size()
-                    << "\nNumero de Links Unicos = " << link_unicos << "| Links grafo: " << seen_arestas.size()
-                        << "\n--------------";
- 
-    //TESTE cout << hop_from << " -> " << hop_to << endl;    
+    cout << "==============="
+            <<"\nCarga de Dados inserida com sucesso, roteamento inicializado!" 
+                << "\nNumero de Vertices Unicos (IPs) : " << vert_unicos     
+                    << "\nNumero de Arestas Unicas (Rotas) : = " << link_unicos
+                        << endl;
 
+    cout << "\n===== MENU ====="
+            << "\n1. Exibir grafo completo"
+                << "\n2. Encontrar menor caminho"
+                    << "\n3. Calcular diametro do grafo"
+                        << "\n4. Identificar roteadores criticos"
+                            << "\n0. Sair"
+                                << "\n==============" << endl;
+ 
+    int op;
+    while (cin >> op){
+        if (op == 0){ cout << "Saindo..." << endl; break; }
+            
+        switch(op){
+            //EXIBIR GRAFO
+            case 1: {
+                int metodoSaida;
+                do {
+                    cout << "Selecione o formato de saída do Grafo:" 
+                            << "\n1. Tela" << "\n2. Imagem (PNG)" <<"\n3. Documento (PDF)" << "\n Ou '0' para sair" << endl;
+
+                    cin >> metodoSaida;   
+                    if (metodoSaida == 0) break;
+
+                    g.export2dot("rotas.dot");            
+
+                    switch(metodoSaida){
+                        case 1: // TELA
+                            cout << "\nAbrindo visualizacao com Graphviz (dot -Tx11)...\n";
+                            std::system("dot -Tx11 rotas.dot");
+                            break;
+
+                        case 2: // PNG
+                            cout << "\nExportando para PNG..\n" << endl;
+                            system("dot -Tpng rotas.dot -o ROTAS_TELA.png");
+                            system("start ROTAS_TELA.png");       
+                            break;
+
+                        case 3: // PDF
+                            cout << "\nExportando para PDF...\n";
+                            std::system("dot -Tpdf rotas.dot -o PDF_ROTAS.pdf");
+                            cout << "Arquivo PDF_ROTAS.pdf gerado!\n";
+                            break;
+
+                        default:
+                            cout <<"\nComando Inválido! Tente novamente.\n";
+                    }
+                } while (metodoSaida !=0);
+            break;
+            }
+
+            case 2: //ENCONTRAR MENOR CAMINHO
+            break;
+
+            case 3: //DIAMETRO GRAFO
+            break;
+
+            case 4: //ENCONTRAR ROTEADORES CRITICOS
+            break;
+        }
+
+    }
     return 0;
 }
